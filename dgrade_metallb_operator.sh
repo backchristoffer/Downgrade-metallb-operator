e0247b305e342fd56e530e881668ab5bbc6ca296
backchristoffer <backchristoffer@hotmail.com>
2024-04-26 13:40:58 +0200

    script to downgrade metallb-operator

---
+++ dgrade_metallb_operator.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+NAMESPACE="metallb-system"
+NEW_CSV="metallb-operator.4.12.0-202311220908"
+CURRENT_CSV=$(oc get csv -n "$NAMESPACE" --no-headers | awk '{print $1}')
+
+
+SUBSCRIPTION_YAML='
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: metallb-operator
+  namespace: metallb-system
+spec:
+  channel: stable
+  installPlanApproval: Manual
+  name: metallb-operator
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+  startingCSV: '"$NEW_CSV"'
+'
+echo "Installed CSV:"
+echo $CURRENT_CSV
+echo "Deleting current CSV..."
+oc delete csv -n "$NAMESPACE" $CURRENT_CSV &> /dev/null
+echo "Deleting current Subscription..."
+oc delete subscription -n "$NAMESPACE" metallb-operator &> /dev/null
+echo "Deleting InstallPlans..."
+oc delete installplan -n "$NAMESPACE" --all &> /dev/null
+
+echo "Applying new Subscription..."
+echo "$SUBSCRIPTION_YAML" | oc apply -f - &> /dev/null
+
+sleep 10
+
+INSTALLPLAN=$(oc get installplan -n "$NAMESPACE" --no-headers | awk '{print $1}')
+if [ -n "$INSTALLPLAN" ]; then
+    echo "Approving InstallPlan..."
+    oc patch installplan "$INSTALLPLAN" -n "$NAMESPACE" --type merge -p '{"spec":{"approved":true}}' &> /dev/null
+fi
+
+echo "Script completed successfully!"
+echo "Installed CSV (after script execution):"
+echo $CURRENT_CSV
